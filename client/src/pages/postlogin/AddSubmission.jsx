afa323ab61e08b5d56a260749a84208d285528b7
vishxlkr <vishxlkr@gmail.com>
2025-11-02 20:52:18 +0530

    update

--- client/src/pages/postlogin/AddSubmission.jsx
+++ client/src/pages/postlogin/AddSubmission.jsx
@@ -247,4 +247,4 @@ export default AddSubmission;
 
 git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
 git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quick
+git quickgit quick

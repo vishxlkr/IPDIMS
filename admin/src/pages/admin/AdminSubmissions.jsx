1963c8b683fb8252eb51057de431da9508ad4608
vishxlkr <vishxlkr@gmail.com>
2025-11-10 13:52:35 +0530

    update

--- admin/src/pages/admin/AdminSubmissions.jsx
+++ admin/src/pages/admin/AdminSubmissions.jsx
@@ -1033,7 +1033,7 @@ const AdminSubmissions = () => {
                         <select
                            value={selectedReviewer}
                            onChange={(e) => setSelectedReviewer(e.target.value)}
-                           className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                           className="w-full px-4 py-3 text-black border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                         >
                            <option value="">-- Select a Reviewer --</option>
                            {reviewers

84b467e039a1cccdfd2d38950c50df03b82aa75e
vishxlkr <vishxlkr@gmail.com>
2025-12-01 22:49:31 +0530

    update

--- api/index.js
+++
@@ -1,5 +0,0 @@
-import app from "../server/server.js";
-
-export default function handler(req, res) {
-   return app(req, res);
-}
--- new.js
+++ new.js
@@ -2,3 +2,5 @@ https://ipdims-git-main-vishxlkrs-projects.vercel.app/
 
 
 https://ipdims.vercel.app/
+
+git quick

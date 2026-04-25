c9d100fa9885204c19fce34262b8b93b09873b21
vishxlkr <vishxlkr@gmail.com>
2026-04-25 20:13:08 +0530

    update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/ipdims-admin/",
+   base: "/",
    plugins: [react(), tailwindcss()],
    resolve: {
       alias: {

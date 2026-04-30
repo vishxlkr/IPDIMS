6e3aa244b1a6310d2843161b14b53f4c63dfca12
vishxlkr <vishxlkr@gmail.com>
2026-04-30 11:29:47 +0530

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-   <HashRouter>
+   <BrowserRouter>
       <AppContextProvider>
          <App />
       </AppContextProvider>
-   </HashRouter>,
+   </BrowserRouter>,
 );

8716b7e9d6db7ff403b3712bdee83151eeba6b3c
vishxlkr <vishxlkr@gmail.com>
2026-04-30 11:18:54 +0530

    update

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,16 +2,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AdminContextProvider, { AdminContext } from "./context/AdminContext.jsx";
 import ReviewerContextProvider from "./context/ReviewerContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-   <BrowserRouter>
+   <HashRouter>
       <AdminContextProvider>
          <ReviewerContextProvider>
             <App />
          </ReviewerContextProvider>
       </AdminContextProvider>
-   </BrowserRouter>
+   </HashRouter>,
 );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-   <BrowserRouter>
+   <HashRouter>
       <AppContextProvider>
          <App />
       </AppContextProvider>
-   </BrowserRouter>
+   </HashRouter>,
 );
